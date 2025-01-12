cf542bae2be4489986e5561d13bd76fe1da0fab7
Alina <agavrikova.s@yandex.ru>
2025-01-13 00:41:11 +0300

    немного изменила функции для аримы

--- ml_models/preprocessing.py
+++ ml_models/preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from statsmodels.tsa.stattools import adfuller
+from pmdarima import auto_arima
 
 
 class Preprocessing:
@@ -10,34 +11,58 @@ class Preprocessing:
     def __init__(self, ts):
         self.ts = ts
 
-
-    def check_stationarity(self):
+    @staticmethod
+    def check_stationarity(ts):
         """
-            Проверка ВР на стационарность. 
+            Проверка ВР на стационарность.
             p-value < 0.05 свидетельствует о стационарности ВР.
             p-value > 0.05 свидетельствует о плохой стационарности ВР.
         """
-        result = adfuller(self.ts)
+        result = adfuller(ts)
         return result[1] < 0.05
-    
 
-    def make_stationary(self):
+    @staticmethod
+    def make_stationary(ts):
         """
             Приведение нестационарного ряда к стационарному виду путём дифференцирования.
             Удаление тренда из ВР.
         """
-        return self.ts.diff().dropna()
+        return ts.diff().dropna()
 
-
-    def inverse_difference(self, last_observation):
+    @staticmethod
+    def inverse_difference(forecast, last_observation):
         """
             Приведение ВР к размерности исходного вида с использованием кумулятивной суммы.
             Args:
                 last_observation: последние фактические данные в DataFrame.
         """
-        return self.ts.cumsum() + last_observation
-    
-    
+        return forecast.cumsum() + last_observation
+
+    # def check_stationarity(self):
+    #     """
+    #         Проверка ВР на стационарность.
+    #         p-value < 0.05 свидетельствует о стационарности ВР.
+    #         p-value > 0.05 свидетельствует о плохой стационарности ВР.
+    #     """
+    #     result = adfuller(self.ts)
+    #     return result[1] < 0.05
+    #
+    # def make_stationary(self):
+    #     """
+    #         Приведение нестационарного ряда к стационарному виду путём дифференцирования.
+    #         Удаление тренда из ВР.
+    #     """
+    #     return self.ts.diff().dropna()
+    #
+    # def inverse_difference(self, last_observation):
+    #     """
+    #         Приведение ВР к размерности исходного вида с использованием кумулятивной суммы.
+    #         Args:
+    #             last_observation: последние фактические данные в DataFrame.
+    #     """
+    #     return self.ts.cumsum() + last_observation
+
+
     def search_last_fact_data(self):
         """
             Нахождение последнего фактического месяца и года в DataFrame.
@@ -45,4 +70,4 @@ class Preprocessing:
         last_date = self.ts.index.max()
         end_year = last_date.year
         last_month = last_date.month
-        return end_year, last_month
+        return end_year, last_month, last_date
